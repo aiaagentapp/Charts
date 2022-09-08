329d6b308e6930bad045e5d3a2c8adf511a38482
aiaagentapp <49463797+aiaagentapp@users.noreply.github.com>
2022-09-08 13:33:14 +0800

    Update ChartDataSet.swift

--- Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
+++ Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
@@ -427,11 +427,6 @@ extension ChartDataSet: MutableCollection {
             entries[position] = newValue
         }
     }
-    
-    public func replaceSubrange<C>(_ subrange: Swift.Range<Index>, with newElements: C) where C : Collection, Element == C.Element { 
-     entries.replaceSubrange(subrange, with: newElements) 
-     notifyDataSetChanged() 
-    } 
 }
 
 // MARK: RandomAccessCollection
